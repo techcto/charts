aef5e0b2fd0215c3eed077b43bbd86c00be7b348
techcto <smoore@solodev.com>
2019-09-08 09:18:14 -0400

    ..

--- cmd.sh
+++ cmd.sh
@@ -4,13 +4,12 @@ export $(egrep -v '^#' .env | xargs)
 args=("$@")
 
 tag(){
-    release
     VERSION="${args[1]}"
     git tag -a v${VERSION} -m "tag release"
     git push --tags
 }
 
-release(){
+update(){
     #https://blog.softwaremill.com/hosting-helm-private-repository-from-github-ff3fa940d0b7
     helm package solodev-dcx
     helm repo index .
