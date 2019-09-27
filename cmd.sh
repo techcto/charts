7290155ec95346ca6be7740a4c4d33c3b8899df2
techcto <smoore@solodev.com>
2019-09-27 17:38:17 -0400

    portworx

--- cmd.sh
+++ cmd.sh
@@ -13,6 +13,7 @@ update(){
     helm package solodev-network
     helm package solodev-dcx
     helm package solodev-dcx-aws
+    helm package portworx
     helm repo index .
     helm repo update
 }
